ee440fabce8291902430d491eb5f98581373dc70
Quan <quantrung286@gmail.com>
2022-03-11 00:27:44 +0700

    up

--- src/components/Discovery/Discovery.jsx
+++ src/components/Discovery/Discovery.jsx
@@ -8,6 +8,7 @@ import { getDiscoveryVideos } from '../../api'
 import { Navbar } from '../index'
 import HlsPlayer from 'react-hls-player'
 import { InView } from 'react-intersection-observer'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 const Discovery = () => {
 	const {
@@ -28,9 +29,9 @@ const Discovery = () => {
 
 	useEffect(() => {
 		getDiscoveryVideos(page).then((res) => {
-			setDicoveryVideos(res)
+			setDicoveryVideos(dicoveryVideos.concat(res))
 		})
-	}, [])
+	}, [page])
 
 	console.log(dicoveryVideos)
 
@@ -38,39 +39,50 @@ const Discovery = () => {
 		<React.Fragment>
 			<Navbar />
 			<div className={disContainer}>
-				{dicoveryVideos.map((post) => (
-					<div key={post.id} className={postContainer}>
-						<div className={postImageSection}>
-							<img
-								className={postUpImage}
-								src={post.upInfo.upImgUrl || noImage}
-								alt={post.upInfo.upName}
-								effect='opacity'
-							/>
-						</div>
-						<div className={postContentSection}>
-							<p className={postUpName}>{post.upInfo.upName}</p>
-							<p className={postCaption}>{post.introduction}</p>
-							<div className={postVideoContainer}>
-								<InView threshold={0.5}>
-									{({ inView, ref }) => (
-										<div ref={ref}>
-											<HlsPlayer
-												
-												className={postVideo}
-												controls
-												autoPlay={inView}
-												playsInline
-												src={post.mediaUrl}
-											/>
-										</div>
-									)}
-								</InView>
+				<InfiniteScroll
+					dataLength={dicoveryVideos.length}
+					next={() => setPage((prev) => prev + 1)}
+					hasMore={true}
+					loader={<h4>Loading...</h4>}
+					endMessage={
+						<p style={{ textAlign: 'center' }}>
+							<b>Yay! You have seen it all</b>
+						</p>
+					}
+				>
+					{dicoveryVideos.map((post) => (
+						<div key={post.id} className={postContainer}>
+							<div className={postImageSection}>
+								<img
+									className={postUpImage}
+									src={post.upInfo.upImgUrl || noImage}
+									alt={post.upInfo.upName}
+									effect='opacity'
+								/>
 							</div>
+							<div className={postContentSection}>
+								<p className={postUpName}>{post.upInfo.upName}</p>
+								<p className={postCaption}>{post.introduction}</p>
+								<div className={postVideoContainer}>
+									<InView threshold={0.5}>
+										{({ inView, ref }) => (
+											<div ref={ref}>
+												<HlsPlayer
+													className={postVideo}
+													controls
+													autoPlay={inView}
+													playsInline
+													src={post.mediaUrl}
+												/>
+											</div>
+										)}
+									</InView>
+								</div>
+							</div>
+							<div className={postReact}></div>
 						</div>
-						<div className={postReact}></div>
-					</div>
-				))}
+					))}
+				</InfiniteScroll>
 			</div>
 		</React.Fragment>
 	)
