3e8c24dd7d22b432c7cf9395ba720d19e4e125d4
Quan Trung <quantrung286@gmail.com>
2022-05-12 17:41:38 +0700

    design

--- src/pages/Movie/Movie.jsx
+++ src/pages/Movie/Movie.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { Player } from 'react-tuby'
+import 'react-tuby/css/main.css'
 
 import styles from './Movie.module.css'
 
@@ -8,19 +10,40 @@ import { getMovieDetail } from '../../api'
 import { Navbar, ScrollToTop } from '../../components'
 
 const Movie = () => {
-	const { movieContainer } = styles
+	const {
+		movieContainer,
+		movieMain,
+		movieSection,
+		videoSection,
+		relativeSection,
+		sameSeriesSection,
+		relativeSeriesSection,
+	} = styles
 
+	const [movie, setMovie] = useState(null)
+	console.log(movie)
 	const params = useParams()
 
 	useEffect(async () => {
 		const fetchData = await getMovieDetail(params.movieId, 0)
 
-
+		if (fetchData) {
+			setMovie(fetchData)
+		}
 	}, [])
 
 	return (
 		<div className={movieContainer}>
 			<Navbar />
+			<div className={movieMain}>
+				<div className={movieSection}>
+					<div className={videoSection}></div>
+					<div className={relativeSection}>
+						<div className={sameSeriesSection}></div>
+						<div className={relativeSeriesSection}></div>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
