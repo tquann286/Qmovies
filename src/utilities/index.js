84bceb7551ec702d58cb146c08f1cfd9cd63a447
Quan Trung <quantrung286@gmail.com>
2022-06-03 14:52:42 +0700

    up

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,3 +1,4 @@
 export { default as navbarMenuData } from './navbarMenuData'
 export { default as onScrollToTop } from './scrollToTop'
 export { default as detectMovieLink } from './detectMovieLink'
+export { default as subtitleProxy } from './subtitleProxy'
