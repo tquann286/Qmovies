84aa635bb4e8c1d2a24a9803f443aff63ce7cf37
Quan Trung <quantrung286@gmail.com>
2022-06-09 16:13:28 +0700

    up

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+
 import styles from './SignIn.module.css'
+import { LogoIcon } from '../../components'
+
 import { auth, provider } from '../../shared/firebase'
 import { signInWithPopup } from 'firebase/auth'
+import {FcGoogle} from 'react-icons/fc'
+import {ImFacebook} from 'react-icons/im'
 
 const SignIn = () => {
-	const { container, main, signInBtn } = styles
+	const { container, logoIcon, main, signInGoogleBtn, signInSection, signInFacebookBtn } = styles
 
 	const signInWithGoogle = () => {
 		signInWithPopup(auth, provider)
@@ -16,17 +21,32 @@ const SignIn = () => {
 				console.log(err)
 			})
 	}
+
+	const signInWithFacebook = () => {
+		
+	}
+
 	return (
 		<div className={container}>
 			<div className={main}>
+				<div className={logoIcon}>
+					<LogoIcon />
+				</div>
 				<svg xmlns='http://www.w3.org/2000/svg'>
 					<filter id='motion-blur-filter' filterUnits='userSpaceOnUse'>
 						<feGaussianBlur stdDeviation='100 0'></feGaussianBlur>
 					</filter>
 				</svg>
 				<span filter-content='S'>Sign In</span>
-				<div className={signInBtn} onClick={signInWithGoogle}>
-					Sign In
+				<div className={signInSection}>
+					<button className={signInGoogleBtn} onClick={signInWithGoogle}>
+					<FcGoogle />
+						<p>Sign In With Google</p>
+					</button>
+					<button className={signInFacebookBtn} onClick={signInWithFacebook}>
+					<ImFacebook color='white'/>
+						<p>Sign In With Facebook</p>
+					</button>
 				</div>
 			</div>
 		</div>
